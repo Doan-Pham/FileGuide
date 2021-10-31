b46ad4dbcffa5ea9e5fcadf8590de7a930806973
Doan-Pham <Doan-Pham@users.noreply.github.com>
2021-10-31 21:04:44 +0700

    Bug Fixed: Old code not cover all cases : path is file - path is folder - path not exist

--- FileGuide/FrmMain.cs
+++ FileGuide/FrmMain.cs
@@ -26,6 +26,7 @@ namespace FileGuide
         private void Form1_Load(object sender, EventArgs e)
         {
             clsTreeListView.CreateTreeView(this.treeView);
+            tscmbPath.Size = new Size(tsPath.Size.Width - 200, 25);
         }
 
         /// <summary>
@@ -53,27 +54,35 @@ namespace FileGuide
                     // Chỉ xử lý khi đường dẫn không trống
                     if (tscmbPath.Text != "")
                     {
-                        FileInfo file = new FileInfo(tscmbPath.Text.Trim());
-                        // Kiểm tra 
-                        if (file.Exists)
+
+                        // Nếu đường dẫn trỏ đến file thì mở file
+                        if (File.Exists(tscmbPath.Text.Trim()))
                         {
+                            FileInfo file = new FileInfo(tscmbPath.Text.Trim());
                             System.Diagnostics.Process.Start(tscmbPath.Text.Trim());
                             DirectoryInfo parent = file.Directory;
                             tscmbPath.Text = parent.FullName;
                         }
-                        // Nếu không tìm thấy đường dẫn thì báo lỗi
-                        else
+
+                        // Nếu đường dẫn trỏ đến folder thì hiện nội dung folder lên listView
+                        else if (Directory.Exists(tscmbPath.Text.Trim()))
                         {
                             clsTreeListView.ShowContent(this.listView, tscmbPath.Text);
                         }
-                    }
 
+                        // Nếu đường dẫn không tồn tại thì báo lỗi
+                        else
+                        {
+                            MessageBox.Show("File/Folder not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); ;
+                        }
+                    }
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show("Fuck you");
+                    MessageBox.Show(ex.ToString());
                 }
             }
         }
+
     }
 }
